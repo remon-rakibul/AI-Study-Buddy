c41c2634d00b7d7c2e08c05e641246e071a2fbf4
remon-rakibul <remon.rakibul.star@gmail.com>
2024-03-07 16:49:10 +0600

    fix: file bug solved

--- st_backend.py
+++ st_backend.py
@@ -36,7 +36,7 @@ uploaded_files = st.file_uploader(label='Upload Study Material',
 
 # Show Delete Study Materials button
 delete_uploaded_files_btn = st.button("Delete Study Materials")
-generate_questions_btn = st.button("Generate Questions")
+# generate_questions_btn = st.button("Generate Questions")
 # Create submit button
 summary_gen = st.button('Generate Summary')
 
@@ -47,121 +47,119 @@ if delete_uploaded_files_btn:
 
 # Check if files are uploaded
 if uploaded_files:
-    if generate_questions_btn:
-        # Process files
-        files = process_file_multi_docs(uploaded_files)
+    # if generate_questions_btn:
+    # Process files
+    files = process_file_multi_docs(uploaded_files)
 
-        # Load uploaded file
-        data = load_data_multiple_docs(files)
+    # Load uploaded file
+    data = load_data_multiple_docs(files)
 
-        # Split doc for question gen
-        documents_for_question_gen = split_text_multiple_docs(data=data, chunk_size=700, chunk_overlap=50)
+    # Split doc for question gen
+    documents_for_question_gen = split_text_multiple_docs(data=data, chunk_size=700, chunk_overlap=50)
 
-        # Split doc for summary gen
-        documents_for_summary_gen = split_text_multiple_docs(data=data, chunk_size=800, chunk_overlap=80)
+    # Split doc for summary gen
+    documents_for_summary_gen = split_text_multiple_docs(data=data, chunk_size=800, chunk_overlap=80)
+    
+    # Split docs for question asnwering
+    documents_for_question_ans = split_text_multiple_docs(data=data, chunk_size=400, chunk_overlap=50)
+
+    # st.write(documents_for_question_gen)
+    # st.write('Number of documents for question generation: ', len(documents_for_question_gen))
+    # st.write(documents_for_question_ans)
+    # st.write('Number of documents for question answering: ', len(documents_for_question_ans))
+
+    # init llm for question reneration
+    llm_question_gen = initialize_llm(model='gpt-3.5-turbo', temperature=0.4)
+
+    # init llm for question reneration
+    llm_summary_gen = initialize_llm(model='gpt-3.5-turbo', temperature=0.6, stream=True)
+
+    # init llm for question answering
+    llm_question_ans = initialize_llm(model='gpt-3.5-turbo', temperature=0.1)
+
+    # Check If questions is empty
+    if st.session_state['questions'] == 'empty':
+        # Generate questions
+        with st.spinner('Generating questions. This may take a while. Please wait.'):
+            st.session_state['questions'] = generate_questions(llm=llm_question_gen, chain_type='refine', documents=documents_for_question_gen)
+
+    # Add generated question to session state
+    if st.session_state['questions'] != 'empty':
         
-        # Split docs for question asnwering
-        documents_for_question_ans = split_text_multiple_docs(data=data, chunk_size=400, chunk_overlap=50)
+        # Show questions
+        st.info(st.session_state['questions'])
 
-        # st.write(documents_for_question_gen)
-        # st.write('Number of documents for question generation: ', len(documents_for_question_gen))
-        # st.write(documents_for_question_ans)
-        # st.write('Number of documents for question answering: ', len(documents_for_question_ans))
+        # Convert questions to a list of questions
+        st.session_state['questions_list'] = st.session_state['questions'].split('\n')
 
-        # init llm for question reneration
-        llm_question_gen = initialize_llm(model='gpt-3.5-turbo', temperature=0.4)
+        # Multiselect option for selecting questions
+        with st.form(key='my_form'):
 
-        # init llm for question reneration
-        llm_summary_gen = initialize_llm(model='gpt-3.5-turbo', temperature=0.6, stream=True)
+            # Add selected question to a session state
+            st.session_state['questions_to_answer'] = st.multiselect(label='Select Questions to Answer', options=st.session_state['questions_list'])
 
-        # init llm for question answering
-        llm_question_ans = initialize_llm(model='gpt-3.5-turbo', temperature=0.1)
+            # Create submit button
+            submitted = st.form_submit_button('Generate Answer')
 
-        # Check If questions is empty
-        if st.session_state['questions'] == 'empty':
-            # Generate questions
-            with st.spinner('Generating questions. This may take a while. Please wait.'):
-                st.session_state['questions'] = generate_questions(llm=llm_question_gen, chain_type='refine', documents=documents_for_question_gen)
+            # Check if questions are submitted for generating answer
+            if submitted:
 
-        # Add generated question to session state
-        if st.session_state['questions'] != 'empty':
-            
-            # Show questions
-            st.info(st.session_state['questions'])
+                # Change session state value
+                st.session_state['submitted'] = True
 
-            # Convert questions to a list of questions
-            st.session_state['questions_list'] = st.session_state['questions'].split('\n')
+        # Check if questions are submitted
+        if st.session_state['submitted']:
 
-            # Multiselect option for selecting questions
-            with st.form(key='my_form'):
+            # Show spinner while generating answers
+            with st.spinner('Generating Answers. Please wait.'):
 
-                # Add selected question to a session state
-                st.session_state['questions_to_answer'] = st.multiselect(label='Select Questions to Answer', options=st.session_state['questions_list'])
+                # Generate answers for submitted questions
+                generate_answer_chain = create_retrieval_qa_chain(documents=documents_for_question_ans, llm=llm_question_ans)
 
-                # Create submit button
-                submitted = st.form_submit_button('Generate Answer')
-    
-                # Check if questions are submitted for generating answer
-                if submitted:
+                # Loop through all the questions
+                for question in st.session_state['questions_to_answer']:
+                    
+                    # Generate answer for each question
+                    # ans = generate_answer_chain.run(question)
+                    response = generate_answer_chain(question)
 
-                    # Change session state value
-                    st.session_state['submitted'] = True
+                    # Show question
+                    st.write(f'Question: {question}')
 
-            # Check if questions are submitted
-            if st.session_state['submitted']:
+                    # Show answer
+                    st.info(f"Answer: {response['result']}")
 
-                # Show spinner while generating answers
-                with st.spinner('Generating Answers. Please wait.'):
+                    # Show sources of answer
+                    st.caption("Sources:")
 
-                    # Generate answers for submitted questions
-                    generate_answer_chain = create_retrieval_qa_chain(documents=documents_for_question_ans, llm=llm_question_ans)
+                    # Initialize a set to keep unique sources                    
+                    sources = set()
 
-                    # Loop through all the questions
-                    for question in st.session_state['questions_to_answer']:
-                        
-                        # Generate answer for each question
-                        # ans = generate_answer_chain.run(question)
-                        response = generate_answer_chain(question)
+                    # Loop through sources
+                    for source in response['source_documents']:
 
-                        # Show question
-                        st.write(f'Question: {question}')
+                        # Add source to initialized set
+                        sources.add(source.metadata['source'])
 
-                        # Show answer
-                        st.info(f"Answer: {response['result']}")
+                    # Loop through unique sources
+                    for source in sources:
 
-                        # Show sources of answer
-                        st.caption("Sources:")
+                        # Show source
+                        st.caption(source)
 
-                        # Initialize a set to keep unique sources                    
-                        sources = set()
+                    # Show first source document
+                    st.warning(response['source_documents'][0].page_content)
 
-                        # Loop through sources
-                        for source in response['source_documents']:
-
-                            # Add source to initialized set
-                            sources.add(source.metadata['source'])
-
-                        # Loop through unique sources
-                        for source in sources:
-
-                            # Show source
-                            st.caption(source)
-
-                        # Show first source document
-                        st.warning(response['source_documents'][0].page_content)
-
-                        # Show a divider at the end of each answer
-                        st.divider()
+                    # Show a divider at the end of each answer
+                    st.divider()
     
     
-
-    # Check if questions are submitted for generating answer
     if summary_gen:
-         # Process files
-        files = process_file_multi_docs(uploaded_files)
-
+        # Process files
+        # files = process_file_multi_docs(uploaded_files)
+        # st.write(files)
         # Load uploaded file
-        data = load_data_multiple_docs(files)
+        # data = load_data_multiple_docs(files)
 
         # Split doc for summary gen
         documents_for_summary_gen = split_text_multiple_docs(data=data, chunk_size=800, chunk_overlap=80)
@@ -187,7 +185,7 @@ if uploaded_files:
         pdf.set_font("Arial", size=12)
         pdf.multi_cell(0, 10, text)
         return pdf
-    
+
     if export_as_pdf:
         pdf = text_to_pdf(st.session_state['summary'])
         pdf_output = BytesIO()
@@ -199,6 +197,6 @@ if uploaded_files:
         href = f'<a href="data:application/pdf;base64,{b64}" download="summary.pdf">Download PDF</a>'
         st.markdown(href, unsafe_allow_html=True)
 
-    # summary_gen = False
+            # summary_gen = False
 
 atexit.register(delete_uploaded_files_and_db)
